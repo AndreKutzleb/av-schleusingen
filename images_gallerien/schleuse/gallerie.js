ad6775215321175d67773c25e8b9b8c518ac86db
Andre Kutzleb <andrekutzleb@KrautOs.fritz.box>
2025-05-10 23:40:23 +0200

    Added files

---
+++ images_gallerien/schleuse/gallerie.js
@@ -0,0 +1,52 @@
+// JavaScript Document
+
+//Copyright by Patrick Steinicke 2003 - www.M-SIGNS.de
+//------------------------------------------------------------------
+
+var bilder = new Array
+(
+"images/01.jpg",
+"images/02.jpg",
+"images/03.jpg",
+"images/04.jpg",
+"images/05.jpg",
+"images/06.jpg",
+"images/07.jpg",
+"images/08.jpg"
+);
+
+
+var zeit = 5000;
+var i = 0;
+
+
+
+
+function diavor()
+{
+    i++
+    if (i == bilder.length)
+      {
+         i = 0;
+      }
+	 
+document.dia.src = bilder[i];  
+}
+
+
+function diarueck()
+{
+    i--
+    if (i == -1)
+      {
+         i = bilder.length-1;
+      }
+	 
+document.dia.src = bilder[i];  
+}
+
+
+function zeigen()
+{
+document.diagr.src = grBilder[i];
+}
